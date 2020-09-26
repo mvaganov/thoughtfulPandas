6ad27b4f40eb0f01df990e07372b200df5735279
Michael Vaganov <michael.vaganov@gmail.com>
2020-09-25 20:07:42 -0700

    pieces selected and moved (sloppy!)
    TODO fix pieces

--- Assets/_michael/Scripts/PieceSelector.cs
+++ Assets/_michael/Scripts/PieceSelector.cs
@@ -7,19 +7,49 @@ public class PieceSelector : MonoBehaviour
 	public GameObject visualizer;
 	Camera cam;
 
+	public Piece selected;
+
     void Start()
     {
 		cam = GetComponent<Camera>();
     }
 
-    void Update()
-    {
+	public Piece GetPieceAtMouse(out Vector3 hitLocation)
+	{
 		Ray mouseRay = cam.ScreenPointToRay(Input.mousePosition);
 		RaycastHit whatTheMouseIsHitting = new RaycastHit();
 		bool hitSomething = Physics.Raycast(mouseRay, out whatTheMouseIsHitting);
 		if (hitSomething)
 		{
 			visualizer.transform.position = whatTheMouseIsHitting.point;
+			hitLocation = whatTheMouseIsHitting.point;
+			if (whatTheMouseIsHitting.collider != null)
+			{
+				Piece p = whatTheMouseIsHitting.collider.transform.parent.GetComponent<Piece>();
+				if (p != null)
+				{
+					return p;
+				}
+			}
+		} else {
+			hitLocation = Vector3.zero;
+		}
+		return null;
+	}
+
+	void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector3 spot;
+			Piece p = GetPieceAtMouse(out spot);
+			if(selected == null)
+			{
+				selected = p;
+			} else {
+				selected.transform.position = spot;
+				selected = null;
+			}
 		}
     }
 }
