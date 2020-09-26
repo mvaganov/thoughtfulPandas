e961e0bb6e3fe32673ddfaab32f2a05d07c223ac
Michael Vaganov <michael.vaganov@gmail.com>
2020-09-25 19:58:15 -0700

    raycast code started, collider for pieces

---
+++ Assets/_michael/Scripts/PieceSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceSelector : MonoBehaviour
+{
+	public GameObject visualizer;
+	Camera cam;
+
+    void Start()
+    {
+		cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+		Ray mouseRay = cam.ScreenPointToRay(Input.mousePosition);
+		RaycastHit whatTheMouseIsHitting = new RaycastHit();
+		bool hitSomething = Physics.Raycast(mouseRay, out whatTheMouseIsHitting);
+		if (hitSomething)
+		{
+			visualizer.transform.position = whatTheMouseIsHitting.point;
+		}
+    }
+}
